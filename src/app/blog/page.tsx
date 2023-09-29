194f853c09540a9ef96ebdc6ea7046db213fa59a
dhoonjang <dhoonjang@gmail.com>
2023-09-29 18:06:39 +0900

    fix: remove bg color

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,10 @@
+import { getServerSession } from 'next-auth';
 import { cn } from '@/lib/style';
 
 const Blog = () => {
+  const session = getServerSession();
+  console.log(session);
+
   return <h1 className={cn('font-medium text-large text-left')}>blog</h1>;
 };
 
