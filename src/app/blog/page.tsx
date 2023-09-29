5e19da924fed115caeb2de21dc526fb9aed3ae61
dhoonjang <dhoonjang@gmail.com>
2023-09-29 20:52:43 +0900

    fix: getServerSession authOptions

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,9 @@
 import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 import { cn } from '@/lib/style';
 
-const Blog = () => {
-  const session = getServerSession();
+const Blog = async () => {
+  const session = await getServerSession(authOptions);
   console.log(session);
 
   return <h1 className={cn('font-medium text-large text-left')}>blog</h1>;
